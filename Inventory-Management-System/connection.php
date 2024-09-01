c6fd96dcff42221b7ed28dde50eb009e759a90ff
GOPISETTI GOPI <95520388+gopisettigopi123@users.noreply.github.com>
2024-09-01 14:39:52 +0530

    Update connection.php

--- Inventory-Management-System/connection.php
+++ Inventory-Management-System/connection.php
@@ -5,7 +5,7 @@ $password = "";
 $dbname = "ims480";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn =  mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
